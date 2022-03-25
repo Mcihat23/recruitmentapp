e3d1012a3047d8ace1a8208ebab458c86a207e13
Mcihat23 <mustafacihataksoy@gmail.com>
2022-03-25 21:30:12 +0300

    hashrouter

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,11 +6,11 @@ export default function NavBar() {
     <div>
       <Navbar sticky="top" bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="/">Recruiter</Navbar.Brand>
+          <Navbar.Brand href="#/">Recruiter</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/candidates">Candidates</Nav.Link>
+            <Nav.Link href="#/">Home</Nav.Link>
+            <Nav.Link href="#/about">About</Nav.Link>
+            <Nav.Link href="#/candidates">Candidates</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 import { applyMiddleware, createStore } from "redux";
@@ -20,9 +20,9 @@ const myStore = createStore(userReducer, allExtensions);
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={myStore}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
